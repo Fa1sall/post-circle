d2460fb6f3e0661cbff0a9c867adf5298ca53478
Fa1sall <faisalirfan7c@gmail.com>
2025-10-08 23:58:33 +0530

    feat(posts): update posts model for pagination

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,19 @@
 import pool from "../config/database.js";
 
-export const getAllPosts = async () => {
+export const getPosts = async (offset, limit) => {
   const query = `SELECT u.username, p.title, p.body, p.created_at 
      FROM users AS u INNER JOIN posts AS p 
-     ON u.id = p.user_id;`;
-  const { rows } = await pool.query(query);
+     ON u.id = p.user_id
+     ORDER BY p.created_at DESC
+     LIMIT $1 OFFSET $2;`;
+  const { rows } = await pool.query(query, [offset, limit]);
   return rows;
+};
+
+export const getTotalPosts = async () => {
+  const query = `SELECT COUNT(*) FROM posts;`;
+  const { rows } = await pool.query(query);
+  return parseInt(rows[0].count, 10);
 };
 
 export const createPost = async (user_id, title, body) => {
